4671dc94bb23f6f53dce5ef8d888e6bec5ea10a9
Sumon <sumonece15@gmail.com>
2022-10-19 08:07:53 +0600

    New ChatBot 1.0

---
+++ app/src/main/java/com/sumon/newchatbot/MainActivity.java
@@ -0,0 +1,99 @@
+package com.sumon.newchatbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView chatRV;
+    private EditText userMsgEdt;
+    private FloatingActionButton sendMsgFAB;
+    private final String BOT_KEY = "bot";
+    private final String USER_KEY = "user";
+    private ArrayList<ChatsModal>chatsModalArrayList;
+    private ChatRVAdapter chatRVAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chatRV = findViewById(R.id.idRVChats);
+        userMsgEdt = findViewById(R.id.idEdtMessage);
+        sendMsgFAB = findViewById(R.id.idFABSend);
+
+        chatsModalArrayList = new ArrayList<>();
+        chatRVAdapter = new ChatRVAdapter(chatsModalArrayList,this);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        chatRV.setLayoutManager(manager);
+        chatRV.setAdapter(chatRVAdapter);
+
+        sendMsgFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (userMsgEdt.getText().toString().isEmpty()){
+
+                    Toast.makeText(MainActivity.this,"Please Enter Your Message", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                getResponse(userMsgEdt.getText().toString());
+                userMsgEdt.setText("");
+
+            }
+        });
+    }
+
+    private void getResponse(String message){
+
+        chatsModalArrayList.add(new ChatsModal(message, USER_KEY));
+        chatRVAdapter.notifyDataSetChanged();
+        String url = "http://api.brainshop.ai/get?bid=169904&key=IUBavtkcBuw9Y25p&uid=[uid]&msg="+message;
+        String BASE_URL = "http://api.brainshop.ai/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+        Call<MsgModal> call = retrofitAPI.getMessage(url);
+        call.enqueue(new Callback<MsgModal>() {
+            @Override
+            public void onResponse(Call<MsgModal> call, Response<MsgModal> response) {
+                if (response.isSuccessful()){
+
+                    MsgModal modal = response.body();
+                    chatsModalArrayList.add(new ChatsModal(modal.getCnt(),BOT_KEY));
+                    chatRVAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MsgModal> call, Throwable t) {
+
+                chatsModalArrayList.add(new ChatsModal("Please revert your question", BOT_KEY));
+                chatRVAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+
+    }
+}
